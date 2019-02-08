e0ad1bf21bce95ae8d5b9004100c789af09b69a7
Sashwat K <sashwat0001@gmail.com>
2019-02-08 20:18:46 +0530

    Installer created

---
+++ bash-script/install.sh
@@ -0,0 +1,8 @@
+# installation for ubuntu only
+echo `sudo su`;
+echo `apt update && apt upgrade`;
+echo 'apt install python3';
+echo `apt install python-pip3`;
+echo `pip install tcconfig`;
+echo 'cp notAddict ~/';
+echo "Installation done";
