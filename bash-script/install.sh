a9f0f710699eefe555b75e034c4d1614aa48cff1
Sashwat K <sashwat0001@gmail.com>
2019-02-08 20:21:23 +0530

    sopme changes

--- bash-script/install.sh
+++ bash-script/install.sh
@@ -1,8 +1,7 @@
 # installation for ubuntu only
-echo `sudo su`;
-echo `apt update && apt upgrade`;
-echo 'apt install python3';
-echo `apt install python-pip3`;
+echo `sudo apt update && sudo apt upgrade`;
+echo 'sudo apt install python3';
+echo `sudo apt install python-pip3`;
 echo `pip install tcconfig`;
 echo 'cp notAddict ~/';
 echo "Installation done";
